c04a9086a8441df118f9e66745810a9c8e931bce
Umut Söngüt <1umut.songut@gmail.com>
2022-12-17 13:21:03 +0300

    Add project files.

---
+++ EnBuyukSayi/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnBuyukSayi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new string('-', 70));
+            Console.WriteLine("Sıralayıcıya Hoşgeldiniz! | developed by Mors.");
+            Console.WriteLine(new string('-', 70));
+
+            Console.Write("Kaç Sayılık İşlem Yapmak İstersiniz?: ");
+            int giris = Convert.ToInt32(Console.ReadLine());
+
+            int[] siralama = new int[giris];
+
+            for (int m = 0; m < giris; m++)
+            {
+                Console.Clear();
+                Console.Write((m + 1).ToString() + ".Sayıyı Girin: ");
+                siralama[m] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            Console.WriteLine();
+
+            int enbuyuk;
+            enbuyuk = siralama[0];
+
+            for (int i = 0; i < giris; i++)
+            {
+                if (enbuyuk < siralama[i])
+                {
+                    enbuyuk = siralama[i];
+                }
+            }
+            Console.Write($"En Büyük Sayı:{enbuyuk}");
+
+            Console.Read();
+        }
+    }
+}
